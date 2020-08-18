680e9d94fd3bc07d42882fcf11e5773d16604573
GeoffreyGone <geoffreygone@gmail.com>
2020-08-18 23:29:46 +0000

    Updates

---
+++ precache-manifest.7913b9cf19fa50842947ee2ab863f9ac.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "97fff4193e9f35edca0d3778b1a6040b",
+    "url": "/react-app1/index.html"
+  },
+  {
+    "revision": "9b248653907dce74eb43",
+    "url": "/react-app1/static/css/main.95409c93.chunk.css"
+  },
+  {
+    "revision": "5cb85c849c9e7be3392a",
+    "url": "/react-app1/static/js/2.15bb9b65.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-app1/static/js/2.15bb9b65.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9b248653907dce74eb43",
+    "url": "/react-app1/static/js/main.d63deab8.chunk.js"
+  },
+  {
+    "revision": "230d9e48661d96ec335d",
+    "url": "/react-app1/static/js/runtime-main.be640214.js"
+  },
+  {
+    "revision": "d0e47f14bab4d32e176a8819fd62874c",
+    "url": "/react-app1/static/media/alfred-rowe-FVWTUOIUZd8-unsplash.d0e47f14.jpg"
+  },
+  {
+    "revision": "6d79247865eeea0c46771fd30cc9dfd1",
+    "url": "/react-app1/static/media/alfred-rowe-w2dWzKL_t5E-unsplash.6d792478.jpg"
+  },
+  {
+    "revision": "9f0992a2681d60803436ffb50e2bde5e",
+    "url": "/react-app1/static/media/hello-lightbulb-QqMy7OBBJjE-unsplash.9f0992a2.jpg"
+  },
+  {
+    "revision": "aef205747c7aa14d0f78e6d6700132d2",
+    "url": "/react-app1/static/media/reuben-hayfron-RjkZHESUuPw-unsplash.aef20574.jpg"
+  },
+  {
+    "revision": "beee6df99be415df86e415211ce34769",
+    "url": "/react-app1/static/media/seyiram-kweku-I9YlJppV_TQ-unsplash.beee6df9.jpg"
+  },
+  {
+    "revision": "6d7956e54519589904adc302f47030d1",
+    "url": "/react-app1/static/media/yoel-winkler-bSkAO5691MA-unsplash.6d7956e5.jpg"
+  },
+  {
+    "revision": "0987444b4aad7b77d76fcc363b34f40d",
+    "url": "/react-app1/static/media/zac-wolff-30p3lM7OKzU-unsplash.0987444b.jpg"
+  },
+  {
+    "revision": "a3fbd958a78243827133f15b650b5460",
+    "url": "/react-app1/static/media/zac-wolff-7vU0nE1iwZ4-unsplash.a3fbd958.jpg"
+  }
+]);
